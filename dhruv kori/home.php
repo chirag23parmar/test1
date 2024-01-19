c8e72a2e1e26647e2f9c942abaf2dd5ed4ea795f
chirag23parmar <111501192+chirag23parmar@users.noreply.github.com>
2024-01-19 10:37:39 +0530

    Add files via upload

---
+++ dhruv kori/home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<!-- header section starts -->
+<section class="header">
+    
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+
+</section>
+
+<!-- header section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper css link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="script.js"></script>
+
+</body>
+</html>
